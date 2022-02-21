b0a2b9136fbc33d5130addd0867aa6457be569bc
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-02-21 08:12:22 +0300

    i love coding

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main(){
+    greetings()
+    modulus()
+    sum()
+    funFact()
+}
+fun greetings(){
+    var name= "martin"
+    println("Hello "+name)
+}
+fun modulus(){
+    var num1= 20
+    var num2=7
+    var modulus=(num1 % num2)
+    println(modulus)
+
+
+}
+fun sum(){
+    var a=2
+    var b=4
+    var c=8
+    var d=10
+    var sum=(a+b+c+d)
+    println(sum)
+}
+fun  funFact() {
+    var funFact = "i love cycling"
+    println(funFact)
+}
